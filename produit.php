b25b5f818437fc4522285b53176c87bfda2790f1
KALLE-YOUSSOUF <59064053+KALLE-YOUSSOUF@users.noreply.github.com>
2020-02-26 15:30:04 +0100

    Add files via upload

---
+++ produit.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Produits</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/layout.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/main.css" type="text/css" media="all">
+<script type="text/javascript" src="js/jquery-1.6.js"></script>
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/tms-0.3.js"></script>
+<script type="text/javascript" src="js/tms_presets.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
+
+</head>
+<body id="page2">
+<div class="body1">
+	<div class="main">
+<!--header -->
+		<header>
+			<h1><img src="images/logo.png" width="130" height="90"></h1>
+				
+			<ul class="nav-menu">
+                      <li class="menu-has-children menu-active"><a href="index.php">Accueil </a></li>
+                      <li class="menu-has-children"><a href="#">Qui sommes nous?</a>
+                        <ul>
+                           <li><a href="#">Présentations</a></li>
+                          <li><a href="#">objectifs</a></li>
+                          <li><a href="#">Notre vocation</a></li>
+                        </ul>
+                      </li>
+                      <li class="menu-has-children"><a href="produit.php">Produits</a>
+                        <ul>
+                           <li><a href="#">Production de semence pluviales</a></li>
+                           <li><a href="#">Production de semence de légumes</a></li>
+                           <li><a href="#">Production de fruits</a></li>
+                        
+                        </ul>
+                      </li>
+                      <li><a href="actualite.php">Actualités</a></li>
+                      <li><a href="activite.php">Activités</a></li>
+                      <li><a href="contact.php">Contact</a></li>
+                  </ul>
+		</header>
+<!-- / header -->
+<!-- content -->
+		<article id="content">
+			<div class="wrapper">
+				<h2>Informations utiles</h2>
+				<div class="wrapper">
+					<section class="col1">
+						<span class="dropcap1"><span>01</span></span>
+						<div class="cols">
+							<p class="pad_bot1 color1">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.</p>
+							<p class="pad_bot2">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia.</p>
+							<a href="#" class="link1">Read More</a>
+						</div>
+					</section>
+					<section class="col1 pad_left1">
+						<span class="dropcap1"><span class="color1">02</span></span>
+						<div class="cols">
+							<p class="pad_bot1 color1">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore.</p>
+							<p class="pad_bot2">Cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime.</p>
+							<a href="#" class="link1">Read More</a>
+						</div>
+					</section>
+					<section class="col1 pad_left1">
+						<span class="dropcap1"><span class="color2">03</span></span>
+						<div class="cols">
+							<p class="pad_bot1 color1">Quis autem vel eum iure repre- henderit qui in ea voluptate velit esse quam molestiae.</p>
+							<p class="pad_bot2">Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus.</p>
+							<a href="#" class="link1">Read More</a>
+						</div>
+					</section>
+				</div>
+			</div>
+		</article>
+	</div>
+</div>
+<div class="body2">
+	<div class="main">
+		<article id="content2">
+			<h2>Nos Produits</h2>
+			<div class="wrapper">
+				<div class="cols">
+					<figure class="pad_bot2 pad_top1"><img src="images/fruit1.jpg" alt=""></figure>
+					<p class="pad_bot2">
+						<span class="font1 color2">Fruits de mandarines</span><br>
+						<em>Sed ut perspiciatis unde omnis iste natus error.</em><br>
+					</p>
+					<a href="#" class="link1">Lire plus</a>
+				</div>
+				<div class="cols pad_left1">
+					<figure class="pad_bot2 pad_top1"><img src="images/fruits1.jpg" alt=""></figure>
+					<p class="pad_bot2">
+						<span class="font1 color2">Sweet Pepper</span><br>
+						<em>Oluptatem aclaudntium totam rem aperiam, eaque ipsa.</em><br>
+					</p>
+					<a href="#" class="link1">Lire plus</a>
+				</div>
+				<div class="cols pad_left1">
+					<figure class="pad_bot2 pad_top1"><img src="images/fruis1.jpg" alt=""></figure>
+					<p class="pad_bot2">
+						<span class="font1 color2">Fruits de pomelos</span><br>
+						<em>Eaque ipsa quae ab illo inven- toreuia voluptas.</em><br>
+					</p>
+					<a href="#" class="link1">Lire plus</a>
+				</div>
+				<div class="cols pad_left1">
+					<figure class="pad_bot2 pad_top1"><img src="images/orange1.jpg" alt=""></figure>
+					<p class="pad_bot2">
+						<span class="font1 color2">Fruits orange</span><br>
+						<em>Aspernatur aut odit aut fugit, sed quia consequuntur.</em><br>
+					</p>
+					<a href="#" class="link1">Lire plus</a>
+				</div>
+			</div>
+		</article>
+<!-- / content -->
+	</div>
+</div>
+<div class="main">
+<!-- footer -->
+	<footer>
+		<div class="wrapper">
+			<section class="col2">
+				<div class="wrapper">
+					<section class="col4">
+						<h3>Rejoingnez-nous</h3>
+						<ul id="icons">
+							<li><a href="#"><img src="images/icon1.jpg" alt=""><span>Facebook</span></a></li>
+							<li><a href="#"><img src="images/icon2.jpg" alt=""><span>Twitter</span></a></li>
+							<li><a href="#"><img src="images/icon3.jpg" alt=""><span>Linked In</span></a></li>
+						</ul>
+					</section>
+					
+				</div>
+				
+			</section>
+			<section class="col3 pad_left2">
+				<h3>Email Us</h3>
+				<form id="ContactForm">
+					<div>
+						<div class="wrapper">
+							<span>Your Name:</span>
+							<div class="bg"><input type="text" class="input"></div>
+						</div>
+						<div class="wrapper">
+							<span>Your E-mail:</span>
+							<div class="bg"><input type="text" class="input"></div>
+						</div>
+						<div class="textarea_box">
+							<span>Your Message:</span>
+							<div class="bg2"><textarea name="textarea" cols="1" rows="1"></textarea></div>
+						</div>
+						<a href="#" onClick="document.getElementById('ContactForm').submit()">Submit</a>
+					</div>
+				</form>
+			</section>
+		</div>
+		<!-- {%FOOTER_LINK} -->
+	</footer>
+<!-- / footer -->
+</div>
+</body>
+</html>
